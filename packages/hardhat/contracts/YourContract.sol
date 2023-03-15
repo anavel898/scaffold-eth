942993f5786125662b5b5c5509f982b905b153b8
Ana Velimirovic <ana.velimirovic898@gmail.com>
2023-03-15 11:26:42 +0100

    Edited contract purpose

--- packages/hardhat/contracts/YourContract.sol
+++ packages/hardhat/contracts/YourContract.sol
@@ -9,7 +9,7 @@ contract YourContract {
 
   event SetPurpose(address sender, string purpose);
 
-  string public purpose = "Building Unstoppable Apps!!!";
+  string public purpose = "Ana changed this!!!";
 
   constructor() payable {
     // what should we do on deploy?
